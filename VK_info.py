9fade863238eb9f18533c8e9c72fb196d6b46015
Nadezhda Limanova <dindinka86@gmail.com>
2023-08-08 15:26:09 +0300

    commit

--- VK_info.py
+++ VK_info.py
@@ -39,5 +39,27 @@ class Vk_info_data:
 
 
 
+    """Поиск пары по параметрам"""
+
+
+    def user_search(self, user_info):
+
+        resp = self.vk_user.method('users.search', {
+            'age_from': self.user_info['age'] - 5,
+            'age_to': self.user_info['age'] + 5,
+            'city': self.user_info['city'],
+            'sex': 3 - self.user_info['sex'],
+            'relation': 6,
+            'status': 1,
+            'has_photo': 1,
+            'count': 1000,
+            'v': 5.131})
+        print(resp)
+        if resp:
+            if resp.get('items'):
+                return resp.get('items')
+
+
+
 
 
