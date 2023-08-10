45ddc35d4d457b8eedd04427a767116339f354f5
Tugunova Anastasia <annamir7516@gmail.com>
2023-08-10 14:57:53 +0700

    Added new_file

--- vkinder.py
+++ vkinder.py
@@ -6,7 +6,7 @@ from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 from vk_api import VkUpload
 import datetime
 from pprint import pprint
-from BD.func_BD import check_database, check_table, add_user_database, add_applicant_database, add_favorite_database
+from BD.func_BD import add_user_database, add_applicant_database, add_favorite_database
 import re
 
 
@@ -274,14 +274,3 @@ class VKbot:
 
 
 
-if __name__ == "__main__":
-    check_database()
-    check_table()
-    with open('token.txt', 'r', encoding='utf-8') as file:
-        vk_token = file.read()
-    with open('token_vk.txt', 'r', encoding='utf-8') as file:
-        vk_token_user = file.read()
-    token = vk_token
-    token_user = vk_token_user
-    bot = VKbot(token, token_user)
-    bot.run()
